61057db970c8c38779f6a3644c469d178abe8c74
poh429 <asd46050914@gmail.com>
2025-12-06 12:44:51 +0800

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dodge-game/', // ensure correct asset paths when deployed to GitHub Pages
 })
